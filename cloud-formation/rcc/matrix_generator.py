67fbddb5d23d9f1bfad5d4fe763aef771c7adb22
k8s <k8s@nzrbhome.click>
2025-03-27 11:58:42 +0000

    Еуіе123123

--- cloud-formation/rcc/matrix_generator.py
+++ cloud-formation/rcc/matrix_generator.py
@@ -20,30 +20,35 @@ def generate_matrix(config_path):
         'custom_matrix': {'include': []}
     }
     
+    # Обробка першого (і єдиного) deployments
+    deployment = config.get('deployments', [{}])[0]
+    
     # Обробка конфігурації
-    for deployment in config.get('deployments', []):
-        for env in deployment.get('environments', []):
-            if env not in environments:
-                continue
-            
-            # Збирання параметрів для середовища
-            matrix_item = {
-                'environment': env,
-                'runner': deployment.get('runners', {}).get(env, 'ubuntu-latest'),
-                'github_environment': deployment.get('github_environments', {}).get(env),
-                'aws_region': deployment.get('aws_regions', {}).get(env),
-                'parameters': deployment.get('parameters', {}).get(env, {})
-            }
-            
-            # Додавання до відповідної матриці
-            if env == 'dev':
-                matrices['dev_matrix']['include'].append(matrix_item)
-            elif env == 'int':
-                matrices['int_matrix']['include'].append(matrix_item)
-            elif env == 'prod':
-                matrices['prod_matrix']['include'].append(matrix_item)
-            else:
-                matrices['custom_matrix']['include'].append(matrix_item)
+    for env in deployment.get('environments', []):
+        if env not in environments:
+            continue
+        
+        # Збирання параметрів для середовища
+        matrix_item = {
+            'environment': env,
+            'runner': deployment.get('runners', {}).get(env, 'ubuntu-latest'),
+            'github_environment': deployment.get('github_environments', {}).get(env),
+            'aws_region': deployment.get('aws_regions', {}).get(env),
+            'aws_role_secret': deployment.get('aws_role_secrets', {}).get(env),
+            'cfn_role_secret': deployment.get('cfn_role_secrets', {}).get(env),
+            'iam_role_secret': deployment.get('iam_execution_role_secrets', {}).get(env),
+            'parameters': deployment.get('parameters', {}).get(env, {})
+        }
+        
+        # Додавання до відповідної матриці
+        if env == 'dev':
+            matrices['dev_matrix']['include'].append(matrix_item)
+        elif env == 'int':
+            matrices['int_matrix']['include'].append(matrix_item)
+        elif env == 'prod':
+            matrices['prod_matrix']['include'].append(matrix_item)
+        else:
+            matrices['custom_matrix']['include'].append(matrix_item)
     
     return json.dumps(matrices)
 
